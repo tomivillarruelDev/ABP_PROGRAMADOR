29bb64ce18179b9d1046905398ec245be13ba1a1
Tomas Villarruel <tomasvillarruel18@gmail.com>
2025-05-22 20:00:49 -0300

    fix nombre

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+print("Bienvenidos a SkyRoute - Sistema de Gestión de Pasajes")
+print("1. Gestionar Clientes")
+print("2. Gestionar Destinos")
+print("3. Gestionar Ventas")
+print("4. Consultar Ventas")
+print("5. Botón de Arrepentimiento")
+print("6. Ver Reporte General")
+print("7. Acerca del Sistema")
+print("8. Salir")
